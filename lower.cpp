d7b272385419775e41ef62fe8854858579242244
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-22 22:02:55 -0400

    Create lower.cpp

---
+++ lower.cpp
@@ -0,0 +1,7 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab 4D
+I wrote a program that prints the bottom-left half of a square, given the side length.
+*/
