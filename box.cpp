5c5a563146834b477661aea6bd20535016761902
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-22 22:28:32 -0400

    Update box.cpp

--- box.cpp
+++ box.cpp
@@ -5,3 +5,28 @@ Instructor: Micheal Zamansky
 Assignment: Lab4A
 It asks the user to to input width and height and prints a solid rectangular box of the requested size using asterisks.
 */
+
+# include<iostream>
+using namespace std;
+int main()
+
+{
+int width,height;
+std::cout << "Input width : "<< std::endl;
+std::cin >> width;
+std::cout << "Input height : "<< std::endl;
+std::cin >> height;
+  
+std::cout << "Shape : "<<endl<<endl;
+for(int i=0;i<height;i++)
+{ 
+for(int j=0;j<width;j++)
+{ 
+  std::cout<<"*";
+}
+  std::cout<<endl;
+}
+
+return 0;
+
+}
