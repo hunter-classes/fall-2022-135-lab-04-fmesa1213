7d04f9e80aa3d473de1595da4afc13225672a3af
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-22 22:09:28 -0400

    Create checkerboard3x3.cpp

---
+++ checkerboard3x3.cpp
@@ -0,0 +1,7 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab 4G
+I wrote a program that asks the user to input width and height and prints a checkerboard of 3-by-3 squares.
+*/
