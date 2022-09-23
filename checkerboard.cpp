320fc1a04889c5b12b488a88ba5398dfc5b474d7
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-22 22:34:36 -0400

    Update checkerboard.cpp

--- checkerboard.cpp
+++ checkerboard.cpp
@@ -6,3 +6,31 @@ Assignment: Lab 4B
 I wrote a program that asks the user to input width and height and prints a rectangular checkerboard of the 
 requested size using asterisks and spaces (alternating).
 */
+
+# include<iostream>
+using namespace std;
+int main()
+{
+int width, height;
+std::cout << "Input width: "<< std::endl;
+std::cin >> width;
+std::cout << "Input height: "<< std::endl;
+std::cin >> height;
+
+  for(int row = 0; row < height; row++) 
+{
+  for(int col = 0; col < width; col++) 
+{
+if((row+col) % 2 == 0) 
+{
+  std::cout << "*";
+} 
+else 
+{
+  std::cout << " ";
+}
+}
+  std::cout << std::endl;
+}
+return 0;
+}
