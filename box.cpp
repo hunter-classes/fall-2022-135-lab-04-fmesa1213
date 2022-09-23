9d875bc55b09f2c9e71f5181c2639a9e718ea466
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-22 21:57:39 -0400

    Create box.cpp

---
+++ box.cpp
@@ -0,0 +1,7 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab4A
+It asks the user to to input width and height and prints a solid rectangular box of the requested size using asterisks.
+*/
