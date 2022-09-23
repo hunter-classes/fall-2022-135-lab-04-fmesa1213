78d2d5a9200fb18d0e3eb89d050a7207f90abfab
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-22 21:59:30 -0400

    Create checkerboard.cpp

---
+++ checkerboard.cpp
@@ -0,0 +1,8 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab 4B
+I wrote a program that asks the user to input width and height and prints a rectangular checkerboard of the 
+requested size using asterisks and spaces (alternating).
+*/
