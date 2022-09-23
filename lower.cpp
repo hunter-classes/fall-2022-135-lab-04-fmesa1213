002e187b37329e189b76653de9e1223e303d9d56
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-22 22:44:35 -0400

    Update lower.cpp

--- lower.cpp
+++ lower.cpp
@@ -5,3 +5,26 @@ Instructor: Micheal Zamansky
 Assignment: Lab 4D
 I wrote a program that prints the bottom-left half of a square, given the side length.
 */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int length;
+
+std::cout<<"Input side length: "<< std::endl;
+std::cin>>length;
+
+std::cout<<"Shape: "<< std::endl;
+
+for(int i=1;i<=length;i++)
+{
+for(int j=1;j<=i;j++)
+{
+cout<<"*";
+}
+std::cout<< std::endl;
+}
+  return 0;
+} 
