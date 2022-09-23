28a199f17d5a5ee5a4fd6f086a1d6e1b2dfec22b
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-22 22:00:11 -0400

    Create cross.cpp

---
+++ cross.cpp
@@ -0,0 +1,7 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab 4C
+I wrote a program that asks the user to input the shape size, and prints a diagonal cross of that dimension.
+*/
