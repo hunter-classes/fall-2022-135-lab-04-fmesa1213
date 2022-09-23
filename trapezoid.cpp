f30fea482acee6473bc2e5a4851bcb04b4d6e23b
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-22 22:06:51 -0400

    Create trapezoid.cpp

---
+++ trapezoid.cpp
@@ -0,0 +1,8 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Micheal Zamansky
+Assignment: Lab 4F
+I wrote a program that prints an upside-down trapezoid of given width and height.
+However, if the input height is impossibly large for the given width, then the program should report, "Impossible shape!"
+*/
