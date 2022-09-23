1e9f3764f1cd853c639c2bfb47d627598425c612
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-22 22:52:32 -0400

    Update box.cpp

--- box.cpp
+++ box.cpp
@@ -6,7 +6,7 @@ Assignment: Lab4A
 It asks the user to to input width and height and prints a solid rectangular box of the requested size using asterisks.
 */
 
-# include<iostream>
+#include<iostream>
 using namespace std;
 int main()
 
